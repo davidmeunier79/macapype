bd24694e9dfc40cdf6b3733822ec773a8e424df0
David Meunier <david_meunier_79@hotmail.fr>
2020-05-07 09:34:45 +0200

    test new atlas brex

--- run_examples/run_segment_sphinx_pnh_ants_based.py
+++ run_examples/run_segment_sphinx_pnh_ants_based.py
@@ -32,10 +32,14 @@ if "general" in params.keys() and "template_name" in params["general"].keys():
 else:
     template_name = 'NMT_v1.2'
 
-template_dir = load_test_data(template_name)
+if "general" in params.keys() and "my_path" in params["general"].keys():
+    my_path = params["general"]["my_path"]
+else:
+    my_path = ""
+
+template_dir = load_test_data(template_name, my_path)
 params_template = format_template(template_dir, template_name)
 print (params_template)
-
 
 data_path = load_test_data("data_test_sphinx_pnh")
 
@@ -46,7 +50,7 @@ T2_file = op.join(data_path, "sub-ziggy_T2w.nii")
 # running workflow
 segment_pnh = create_full_segment_pnh_subpipes(params=params,
                                                params_template=params_template,
-                                               name = "test_NodeParams_ziggy")
+                                               name = "test_NodeParams_sphinx")
 segment_pnh.base_dir = data_path
 
 segment_pnh.inputs.inputnode.T1 = T1_file
