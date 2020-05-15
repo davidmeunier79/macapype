9fce12b7e0bb4b76a97f8ab82926e6c166f1f8ca
David Meunier <david_meunier_79@hotmail.fr>
2020-05-15 11:15:02 +0200

    use of multi_params.json and modif BIDSDataGrabberParams

--- macapype/utils/utils_bids.py
+++ macapype/utils/utils_bids.py
@@ -51,3 +51,49 @@ def create_datasource(data_dir, subjects=None, sessions=None, acqs=None):
     bids_datasource.iterables = iterables
 
     return bids_datasource
+
+from .utils_nodes import  BIDSDataGrabberParams
+
+def create_datasource_multi_params(data_dir, multi_params, subjects=None, sessions=None, acqs=None):
+    """ Create a datasource node that have iterables following BIDS format """
+    bids_datasource = pe.Node(
+        interface=BIDSDataGrabberParams(multi_params),
+        #interface=nio.BIDSDataGrabber(),
+        name='bids_datasource'
+    )
+
+    bids_datasource.inputs.base_dir = data_dir
+    bids_datasource.inputs.output_query = {
+        'T1': {
+            "datatype": "anat", "suffix": "T1w",
+            "extensions": ["nii", ".nii.gz"]
+        },
+        'T2': {
+            "datatype": "anat", "suffix": "T2w",
+            "extensions": ["nii", ".nii.gz"]
+        }
+    }
+
+    layout = BIDSLayout(data_dir)
+
+    # Verbose
+    print("BIDS layout:", layout)
+    print("\t", layout.get_subjects())
+    print("\t", layout.get_sessions())
+
+    if subjects is None:
+        subjects = layout.get_subjects()
+
+    if sessions is None:
+        sessions = layout.get_sessions()
+
+    iterables = []
+    iterables.append(('subject', subjects))
+    iterables.append(('session', sessions))
+
+    if acqs is not None:
+        iterables.append(('acquisition', acqs))
+
+    bids_datasource.iterables = iterables
+
+    return bids_datasource
--- macapype/utils/utils_nodes.py
+++ macapype/utils/utils_nodes.py
@@ -1,5 +1,5 @@
 from nipype.pipeline.engine import Node
-
+from nipype.interfaces.base import isdefined
 
 class NodeParams(Node):
 
@@ -25,3 +25,38 @@ class NodeParams(Node):
                 print("Warning, Could not find {} in inputs {} for node {}".
                       format(key, self._interface.inputs, self._name))
             setattr(self._interface.inputs, key, params[key])
+
+
+from nipype.interfaces.io import BIDSDataGrabber
+from .misc import parse_key
+
+class BIDSDataGrabberParams(BIDSDataGrabber):
+    def __init__(self, params={}, **kwargs):
+
+        super(BIDSDataGrabberParams, self).__init__(**kwargs)
+
+        self._params = params
+
+        print(self._params)
+
+    def _set_indiv_params(self, outputs):
+
+        assert "subject" in self._infields and "session" in self._infields, \
+            "Error, subject and session should be defined as iterables"
+
+        keys = ("sub-" + getattr(self.inputs, "subject"),
+               "ses-" + getattr(self.inputs, "session"))
+
+        outputs["indiv_params"] = parse_key(self._params, keys)
+
+        return outputs
+
+    def _list_outputs(self):
+
+        outputs = super(BIDSDataGrabberParams, self)._list_outputs()
+
+        outputs = self._set_indiv_params(outputs)
+
+        return outputs
+
+
