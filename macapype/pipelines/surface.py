fdae575c6835e86ce4dda2f803525163f71157c7
David Meunier <david.meunier@univ-amu.fr>
2025-07-04 13:51:56 +0200

    same for wmgm2mesh

--- macapype/pipelines/surface.py
+++ macapype/pipelines/surface.py
@@ -737,8 +737,10 @@ def create_IsoSurface_brain_pipe(params={},
                                   keep_gcc_bin_mask, "nii_file")
 
     # wmgm2mesh
-    wmgm2mesh = pe.Node(interface=IsoSurface(),
-                        name="wmgm2mesh")
+    wmgm2mesh = NodeParams(
+        interface=IsoSurface(),
+        params=parse_key(params, "wmgm2mesh"),
+        name="wmgm2mesh")
 
     IsoSurface_brain_pipe.connect(keep_gcc_bin_mask, 'gcc_nii_file',
                                   wmgm2mesh, "nii_file")
